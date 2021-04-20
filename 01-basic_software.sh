114ec6fc5a96edc6381dfb9949008145db9b3cbe
Madrid <madrid@max10>
2021-04-20 20:22:43 +0200

    added line breaks

--- 01-basic_software.sh
+++ 01-basic_software.sh
@@ -19,10 +19,10 @@ UNDERLINE=$(tput smul)
 # printf "%40s\n" "${blue}This text is blue${normal}"
 
 
-printf "${BLUE}Actualizando lista de repositorios${NORMAL}"
+printf "${BLUE}Actualizando lista de repositorios${NORMAL}\n"
 sudo apt update
 
-printf "${BLUE}Instalando genie y openshot${NORMAL}"
+printf "${BLUE}Instalando genie y openshot${NORMAL}\n"
 sudo apt install genie openshot -y
 if [ $? ]
 then
@@ -31,7 +31,7 @@ else
   printf "${RED}Ha ocurrido un error${NORMAL}\n\n"
 fi
 
-printf "${BLUE}Instalando snap store y kompozer${NORMAL}"
+printf "${BLUE}Instalando snap store y kompozer${NORMAL}\n"
 sudo apt install snapd -y
 sudo snap install core
 sudo snap install kompozer
@@ -42,16 +42,16 @@ else
   printf "${RED}Ha ocurrido un error${ENDCOLOR}\n\n"
 fi
 
-printf "${BLUE}Creando fichero .desktop para kompozer${NORMAL}"
+printf "${BLUE}Creando fichero .desktop para kompozer${NORMAL}\n"
 sudo cp ./aux/kompozer.desktop /usr/share/applications/
 printf "${GREEN}kompozer deberia ser accesible desde 'Programacion'${NORMAL}\n\n"
 
-printf "${BLUE}Copiando configuracion de Libreoffice base al skel${NORMAL}"
+printf "${BLUE}Copiando configuracion de Libreoffice base al skel${NORMAL}\n"
 sudo mkdir -p /etc/skel/.config/libreoffice/4/user/config/
 sudo cp ./aux/javasettings_Linux_X86_64.xml /etc/skel/.config/libreoffice/4/user/config/
 printf "${GREEN}OK${NORMAL}\n\n"
 
-printf "${BLUE}Instalando grub-customizer${NORMAL}"
+printf "${BLUE}Instalando grub-customizer${NORMAL}\n"
 sudo apt install build-essential cmake libgtkmm-3.0-dev libssl-dev gettext libarchive-dev -y
 wget https://launchpad.net/grub-customizer/5.1/5.1.0/+download/grub-customizer_5.1.0.tar.gz
 tar xzf grub-customizer_5.1.0.tar.gz
@@ -65,9 +65,9 @@ else
   printf "${RED}Ha ocurrido un error${NORMAL}\n\n"
 fi
 
-printf "${BLUE}Lanzando grub-customizer${NORMAL}"
+printf "${BLUE}Lanzando grub-customizer${NORMAL}\n"
 grub-customizer
 
-printf "${GREEN}-----------------------------------${NORMAL}"
-printf "${GREEN}Fin del script${NORMAL}"
-printf "${GREEN}-----------------------------------${NORMAL}"
+printf "${GREEN}-----------------------------------${NORMAL}\n"
+printf "${GREEN}Fin del script${NORMAL}\n"
+printf "${GREEN}-----------------------------------${NORMAL}\n"
